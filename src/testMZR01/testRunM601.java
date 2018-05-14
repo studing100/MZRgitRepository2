ed0f6416c58ba0170fe99ad74d4891254b8f77bf
kyaccp <kyaccp@ky8jf019>
2018-05-14 09:42:20 +0800

    333333

--- src/testMZR01/testRunM601.java
+++ src/testMZR01/testRunM601.java
@@ -3,7 +3,7 @@ package testMZR01;
 public class testRunM601 {
 
 	public static void main(String[] args) {
-		System.out.println("哈哈哈......我的第一次！");
+		System.out.println("哈哈哈......我的第一次！11111111");
 
 	}
 
